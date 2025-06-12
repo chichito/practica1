84a85a9ad12adf87917c566dec992ae973e17cdd
Fernando Cando <fernandocando@hotmail.com>
2025-06-11 21:00:03 -0500

    fer

--- lib/bloc/counter_event.dart
+++ lib/bloc/counter_event.dart
@@ -2,6 +2,7 @@ part of 'counter_bloc.dart';
 
 sealed class CounterEvent {}
 
+//Events
 class IncrementEvent extends CounterEvent {}
 
 class DecrementEvent extends CounterEvent {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:practica1/bloc/counter_bloc.dart';
-import 'package:practica1/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:practica1/bloc/counter_bloc.dart';
+import 'package:practica1/cubit/counter_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -52,6 +52,11 @@ class MyHomePage extends StatelessWidget {
             BlocBuilder<CounterBloc, CounterState>(
               builder: (context, state) {
                 return Text(state.email.toString());
+              },
+            ),
+            BlocBuilder<CounterBloc, CounterState>(
+              builder: (context, state) {
+                return Text(state.counter.toString());
               },
             ),
           ],
